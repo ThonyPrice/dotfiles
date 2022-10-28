baa2c9fd23c4e5cd53fdbd9bc485a9ce6f9f7427
Thony Price <thony@abclabs.se>
2022-10-28 11:28:59 +0200

    macos configurations

---
+++ config-migrations/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias ls="lsd"
+alias ibrew='arch --x86_64 /usr/local/Homebrew/bin/brew'
+
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+# [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
