7cefb3df43c0687a3c5cd04d6bc174431ec7915d
Thony Price <thonyprice@gmail.com>
2022-10-31 10:49:44 +0100

    Add ssh key

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 DOTFILES_DIR="$HOME/dotfiles"
+SSH_DIR="$HOME/.ssh"
+
+if ! [[ -f "$SSH_DIR/id_rsa" ]]; then
+  mkdir -p "$SSH_DIR"
+  chmod 700 "$SSH_DIR"
+  ssh-keygen -b 4096 -t rsa -f "$SSH_DIR/id_rsa" -N "" -C "thonyprice@gmail.com"
+  cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
+  chmod 600 "$SSH_DIR/authorized_keys"
+fi
 
 if ! [ -x "$(command -v ansible)" ]; then
   # TODO: Do not install pip if already installed
