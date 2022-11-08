dc1f562bf2f8abb67b67a72e2de5ce6d727bc3d5
Thony <thonyprice@gmail.com>
2022-11-08 10:10:35 +0100

    Clean up polybar launch script

--- roles/workstation/files/polybar/launch.sh
+++ roles/workstation/files/polybar/launch.sh
@@ -1,39 +1,13 @@
-#!/usr/bin/env sh
+#!/bin/bash
+# Launches an instance of polybar
 
-# More info : https://github.com/jaagr/polybar/wiki
-
-# Install the following applications for polybar and icons in polybar if you are on ArcoLinuxD
-# awesome-terminal-fonts
-# Tip : There are other interesting fonts that provide icons like nerd-fonts-complete
-# --log=error
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-desktop=$(echo $DESKTOP_SESSION)
-count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar --reload mainbar-bspwm -c ~/.config/polybar/config.ini &
 
-
-case $desktop in
-
-    bspwm|/usr/share/xsessions/bspwm)
-    if type "xrandr" > /dev/null; then
-      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config.ini &
-      done
-    else
-    polybar --reload mainbar-bspwm -c ~/.config/polybar/config.ini &
-    fi
-    # second polybar at bottom
-    # if type "xrandr" > /dev/null; then
-    #   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    #     MONITOR=$m polybar --reload mainbar-bspwm-extra -c ~/.config/polybar/config.ini &
-    #   done
-    # else
-    # polybar --reload mainbar-bspwm-extra -c ~/.config/polybar/config.ini &
-    # fi
-    ;;
-
-esac
+echo "Polybar launched..."
