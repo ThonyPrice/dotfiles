c3ff0239cd0093bd01d2428860111f449ce392bb
Thony <thonyprice@gmail.com>
2022-11-08 14:04:49 +0100

    improve bspwm, polybar, and sxhkd configs

--- roles/workstation/files/firefox/user.js
+++ roles/workstation/files/firefox/user.js
@@ -239,6 +239,17 @@ user_pref("layout.css.has-selector.enabled", true);
 ****************************************************************************/
 // see https://github.com/yokoffing/Betterfox/blob/master/Smoothfox.js
 // Enter your scrolling prefs below this line:
+user_pref("general.smoothScroll",                                       true); // DEFAULT
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
+user_pref("general.smoothScroll.msdPhysics.enabled",                    true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",  600);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",      650);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",         25);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",      2.0);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",     250);
+user_pref("general.smoothScroll.currentVelocityWeighting",              1.0);
+user_pref("general.smoothScroll.stopDecelerationWeighting",             1.0);
+user_pref("mousewheel.default.delta_multiplier_y",                      280);
 
 /****************************************************************************
  * START: MY OVERRIDES                                                      *
--- roles/workstation/files/polybar/scripts/updates.sh
+++ roles/workstation/files/polybar/scripts/updates.sh
@@ -3,13 +3,22 @@
 NOTIFY_ICON=/usr/share/icons/arcolinux-candy-beauty/apps/16/albert.svg
 
 update() {
-    UPDATES=$(paru -Qu | wc -l);
+    if ! updates_arch="$(checkupdates | wc -l)"; then
+        updates_arch=0
+    fi
+
+    if ! updates_aur="$(yay -Qum 2>/dev/null | wc -l)"; then
+        updates_aur=0
+    fi
+
+    UPDATES=$((updates_arch + updates_aur))
 }
 
 notify() {
     if command -v notify-send &>/dev/null; then
         if (( UPDATES > 31 )); then
             notify-send -u normal -i $NOTIFY_ICON "$UPDATES New packages"
+            echo
         elif (( UPDATES > 3 )); then
             notify-send -u low -i $NOTIFY_ICON "$UPDATES New packages"
         fi
@@ -22,12 +31,12 @@ while true; do
 
     if (( UPDATES == 0 )); then
         echo " 0"
-        sleep 300
+        sleep 900
     fi
 
     while (( UPDATES > 0 )); do
         echo " $UPDATES"
-        sleep 300
+        sleep 900
         update
     done
 done
