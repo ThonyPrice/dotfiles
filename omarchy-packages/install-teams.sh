f67b9221d80d29f3a53f329313de18efc5e2df8f
Thony Price <thonyprice@gmail.com>
2025-12-01 09:04:52 +0100

    Add teams client

---
+++ omarchy-packages/install-teams.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Zen browser crash when screen sharing teams meetings,
+# use client when team meetings can not be avoided...
+echo "Installing teams client..."
+yay -S --noconfirm --needed teams
